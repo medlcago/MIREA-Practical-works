c75b3f41c18c23364252f57b92b32f566f304423
Alexander <87269586+xMedicago@users.noreply.github.com>
2022-09-26 02:35:21 +0300

    Create Практическая_работа_3_Разветвляющийся_алгоритм.c

---
+++ Практическая_работа_3_Разветвляющийся_алгоритм.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+#include <windows.h>
+
+double f(double x, double y, double z) {
+	return 5 * atan(x) - 1. / 4. * acos(x) * ((x + 3 * fabs(x - y) + pow(x, 2)) / (fabs(x - y) * z + pow(x, 2)));
+}
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	double x, y, z;
+	
+	printf("Введите x: ");
+	scanf_s("%lf", &x);
+
+	printf("Введите y: ");
+	scanf_s("%lf", &y);
+
+	printf("Введите z: ");
+	scanf_s("%lf", &z);
+
+	if (fabs(x - y) * z + pow(x, 2) == 0)
+		printf("ERROR!\n");
+	else
+		printf("%.3lf", f(x, y, z));
+
+	return 0;
+}
