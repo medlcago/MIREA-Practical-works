8b97af171b1eee749a93f1d4f92062c874ef5d58
Alexander <87269586+xMedicago@users.noreply.github.com>
2022-09-26 02:36:47 +0300

    Update Практическая_работа_3_Разветвляющийся_алгоритм.c

--- Практическая_работа_3_Разветвляющийся_алгоритм.c
+++ Практическая_работа_3_Разветвляющийся_алгоритм.c
@@ -3,28 +3,28 @@
 #include <windows.h>
 
 double f(double x, double y, double z) {
-	return 5 * atan(x) - 1. / 4. * acos(x) * ((x + 3 * fabs(x - y) + pow(x, 2)) / (fabs(x - y) * z + pow(x, 2)));
+    return 5 * atan(x) - 1. / 4. * acos(x) * ((x + 3 * fabs(x - y) + pow(x, 2)) / (fabs(x - y) * z + pow(x, 2)));
 }
 
 int main() {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 	
-	double x, y, z;
+    double x, y, z;
 	
-	printf("Введите x: ");
-	scanf_s("%lf", &x);
+    printf("Введите x: ");
+    scanf_s("%lf", &x);
 
-	printf("Введите y: ");
-	scanf_s("%lf", &y);
+    printf("Введите y: ");
+    scanf_s("%lf", &y);
 
-	printf("Введите z: ");
-	scanf_s("%lf", &z);
+    printf("Введите z: ");
+    scanf_s("%lf", &z);
 
-	if (fabs(x - y) * z + pow(x, 2) == 0)
-		printf("ERROR!\n");
-	else
-		printf("%.3lf", f(x, y, z));
+    if (fabs(x - y) * z + pow(x, 2) == 0)
+	printf("ERROR!\n");
+    else
+	printf("%.3lf", f(x, y, z));
 
-	return 0;
+    return 0;
 }
