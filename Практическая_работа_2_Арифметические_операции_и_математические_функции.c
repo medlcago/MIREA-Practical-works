2b5cc90e848a40f1c98971b905d05bbeb183936a
Alexander <87269586+xMedicago@users.noreply.github.com>
2022-09-28 11:17:18 +0300

    Update Практическая_работа_2_Арифметические_операции_и_математические_функции.c

--- Практическая_работа_2_Арифметические_операции_и_математические_функции.c
+++ Практическая_работа_2_Арифметические_операции_и_математические_функции.c
@@ -2,16 +2,12 @@
 #include <math.h>
 #include <windows.h>
 
-double f(double x, double y, double z) {
-    return ((exp(fabs(x - y)) * pow(fabs(x - y), x + y)) / (atan(x) + atan(z))) + pow(pow(x, 6) + pow(log(y), 2), 1./3.);
-}
-
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-	
+
     double x, y, z;
-	
+
     printf("Введите x: ");
     scanf_s("%lf", &x);
 
@@ -21,7 +17,9 @@ int main() {
     printf("Введите z: ");
     scanf_s("%lf", &z);
 
-    printf("%.3lf", f(x, y, z));
+    double result = ((exp(fabs(x - y)) * pow(fabs(x - y), x + y)) / (atan(x) + atan(z))) + pow(pow(x, 6) + pow(log(y), 2), 1. / 3.);
+
+    printf("%.3lf", result);
 
     return 0;
 }
