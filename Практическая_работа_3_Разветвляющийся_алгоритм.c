56076c0d689ba1b020f7cc919f1c01272e61a0e3
Alexander <87269586+xMedicago@users.noreply.github.com>
2022-10-05 20:28:32 +0300

    Update Практическая_работа_3_Разветвляющийся_алгоритм.c

--- Практическая_работа_3_Разветвляющийся_алгоритм.c
+++ Практическая_работа_3_Разветвляющийся_алгоритм.c
@@ -2,28 +2,65 @@
 #include <math.h>
 #include <windows.h>
 
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
-int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+	char ch[2];
+	double x, y, z;
+	double numerator, denominator, element, result;
 
-    double x, y, z;
+	printf("Введите x: ");
+	scanf_s("%lf", &x);
 
-    printf("Введите x: ");
-    scanf_s("%lf", &x);
+	printf("Введите y: ");
+	scanf_s("%lf", &y);
 
-    printf("Введите y: ");
-    scanf_s("%lf", &y);
+	printf("Введите z: ");
+	scanf_s("%lf", &z);
 
-    printf("Введите z: ");
-    scanf_s("%lf", &z);
 
-    if ((fabs(x - y) * z + pow(x, 2) != 0) && (x >= -1 && x <= 1)) {
-        double result = 5 * atan(x) - 1. / 4. * acos(x) * ((x + 3 * fabs(x - y) + pow(x, 2)) / (fabs(x - y) * z + pow(x, 2)));
-        printf("%.3lf", result);
-    }
-    else
-        printf("ERROR!\n");
+	while ((getchar()) != '\n');
+	printf("Введите символ: ");
+	scanf_s("%s", &ch, 2);
+	while ((getchar()) != '\n');
 
-    return 0;
+	numerator = exp(fabs(x - y)) * pow(fabs(x - y), x + y);
+	denominator = atan(x) + atan(z);
+	element = pow(pow(x, 6) + pow(log(y), 2), 1. / 3.);
+
+	result = (numerator / denominator) + element;
+
+	switch (ch[0])
+	{
+	case '+': 
+	case '*': 
+	case '-':
+		if (x == -z || y <= 0)
+		{
+			printf("\nНе выполняется область допустимых значений!\n");
+			break;
+		}
+		else
+		{
+			printf("%.3lf", result);
+			break;
+		}
+
+	case '[': 
+	case ']':
+		printf("%.3lf", result);
+		break;
+
+	case '0':
+		printf("\nВыполнено завершение работы программы.\n");
+		return 0;
+
+	default:
+		printf("\nInvalid data!\n");
+		return 700;
+	}
+
+	return 0;
 }
