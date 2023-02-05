7e90900f9d972a0b15766603ffa9808fe71268e7
Alexander <87269586+medlcago@users.noreply.github.com>
2023-02-05 17:00:20 +0300

    Create Practical_Work_2(1).cpp

---
+++ second_semester/Practical_Work_2(1).cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int n;
+	std::cout << "Введите n: ";
+	std::cin >> n;
+	int* a = new int[n];
+	int* b = new int[n];
+	int c_a = 0, c_b = 0;
+
+	for (int i = 0; i < n; i++) {
+		std::cout << "Введите элемент массива А[" << i << "]: ";
+		std::cin >> a[i];
+		if (a[i] < 0)
+			c_a++;
+
+		std::cout << "Введите элемент массива B[" << i << "]: ";
+		std::cin >> b[i];
+		if (b[i] < 0)
+			c_b++;
+
+		std::cout << "\n";
+	}
+
+	std::cout << "\nКоличество отрицательных элементов в двух массивах: " << c_a + c_b << "\n";
+
+	if (c_a < c_b) {
+		for (int i = 0; i < n; i++)
+			std::cout << a[i] << " ";
+	}
+	else {
+		for (int i = 0; i < n; i++)
+			std::cout << b[i] << " ";
+	}
+
+	return 0;
+}
