5dcac98188dad481421738a21c611b11bda3b02b
Alexander <87269586+medlcago@users.noreply.github.com>
2023-02-21 14:59:47 +0300

    Update Practical_Work_1(2).cpp

--- second_semester/Practical_Work_1(2).cpp
+++ second_semester/Practical_Work_1(2).cpp
@@ -9,6 +9,7 @@ struct Transport
     std::string route;
     float length;
     unsigned int time;
+    std::string date;
 };
 
 int main()
@@ -28,7 +29,7 @@ int main()
 
         std::cout << "Введите маршрут: ";
         std::getline(std::cin, transport[i].route);
-a1:
+    a1:
         std::cout << "Введите протяженность маршрута (км): ";
         while (!(std::cin >> transport[i].length)) {
             std::cout << "Неверные данные! Пожалуйста, введите еще раз!\n";
@@ -38,7 +39,7 @@ a1:
         }
         std::cin.ignore(32767, '\n');
 
- a2:
+    a2:
         std::cout << "Введите время в дороге (мин): ";
         while (!(std::cin >> transport[i].time)) {
             std::cout << "Неверные данные! Пожалуйста, введите еще раз!\n";
@@ -47,21 +48,25 @@ a1:
             goto a2;
         }
         std::cin.ignore(32767, '\n');;
+
+        std::cout << "Введите дату: ";
+        std::getline(std::cin, transport[i].date);
+
     }
-    
+
     // std::setiosflags(std::ios::left)
     // resetiosflags(std::ios::left)
-    
-    std::cout << "|"; std::cout.width(80); std::cout.fill('-'); std::cout << "|"; std::cout << std::endl; std::cout.fill(' ');
-    std::cout.width(80); std::cout << std::setiosflags(std::ios::left) << "| Ведомость общественного транспорта" << std::resetiosflags(std::ios::left); std::cout.fill(' '); std::cout << "|"; std::cout << std::endl; 
-    std::cout << "|"; std::cout.width(80); std::cout.fill('-'); std::cout << "|"; std::cout << std::endl; std::cout.fill(' ');
-    std::cout << "| Вид транспорта | Маршрут | Протяженность маршрута (км) | Время в дороге (мин) |\n";
-    std::cout << "|"; std::cout.width(80); std::cout.fill('-'); std::cout << "|"; std::cout << std::endl; std::cout.fill(' ');
-    for (int i = 0; i < n; i++) {
-        std::cout << "| " << std::setiosflags(std::ios::left) << std::setw(14) << transport[i].type << " | " << std::setw(7) << transport[i].route << " | " << std::setw(27) << transport[i].length << " | " << std::setw(20) << transport[i].time << " |\n" << std::resetiosflags(std::ios::left);
+
+    std::cout << "|"; std::cout.width(100); std::cout.fill('-'); std::cout << "|"; std::cout << std::endl; std::cout.fill(' ');
+    std::cout.width(100); std::cout << std::setiosflags(std::ios::left) << "| Ведомость общественного транспорта" << std::resetiosflags(std::ios::left); std::cout.fill(' '); std::cout << "|"; std::cout << std::endl;
+    std::cout << "|"; std::cout.width(100); std::cout.fill('-'); std::cout << "|"; std::cout << std::endl; std::cout.fill(' ');
+    std::cout << std::setiosflags(std::ios::left) << "| " << std::setw(17) << "Вид транспорта" << " | " << std::setw(12) << "маршрута" << " | " << std::setw(27) << "Протяженность маршрута (км)" << " | " << std::setw(12) << "Время в дороге (мин)" << " | " << std::setw(10) << "Дата" << "|\n" << std::resetiosflags(std::ios::left);
+    std::cout << "|"; std::cout.width(100); std::cout.fill('-'); std::cout << "|"; std::cout << std::endl; std::cout.fill(' ');
+    for (auto& x : transport) {
+        std::cout << std::setiosflags(std::ios::left) << "| " << std::setw(17) << x.type << " | " << std::setw(12) << x.route << " | " << std::setw(27) << std::setprecision(3) << std::fixed << x.length << " | " << std::setw(21) << x.time << "|" << std::setw(11) <<  x.date << "|\n" << std::resetiosflags(std::ios::left);
+        std::cout << "|"; std::cout.width(100); std::cout.fill('-'); std::cout << "|"; std::cout << std::endl; std::cout.fill(' ');
     }
-    std::cout << "|"; std::cout.width(80); std::cout.fill('-'); std::cout << "|"; std::cout << std::endl; std::cout.fill(' ');
-    std::cout.width(80); std::cout << std::setiosflags(std::ios::left) << "| Примечание: Тр - трамвай, Тс - троллейбус, А - автобус" << std::resetiosflags(std::ios::left); std::cout.fill(' '); std::cout << "|"; std::cout << std::endl; std::cout.fill(' ');
-    std::cout << "|"; std::cout.width(80); std::cout.fill('-'); std::cout << "|"; std::cout << std::endl; std::cout.fill(' ');
+    std::cout.width(100); std::cout << std::setiosflags(std::ios::left) << "| Примечание: Тр - трамвай, Тс - троллейбус, А - автобус" << std::resetiosflags(std::ios::left); std::cout.fill(' '); std::cout << "|"; std::cout << std::endl; std::cout.fill(' ');
+    std::cout << "|"; std::cout.width(100); std::cout.fill('-'); std::cout << "|"; std::cout << std::endl; std::cout.fill(' ');
     return 0;
 }
